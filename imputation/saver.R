c9fc10dac9568374fd7e41819ddbe70677defe95
lylamha <lylamha@users.noreply.github.com>
2021-02-03 12:53:44 +0100

    Create saver.R

---
+++ imputation/saver.R
@@ -0,0 +1,46 @@
+library(SAVER)
+library(Seurat)
+
+data_dir <- "./"
+expr_files <- list.files(data_dir, pattern = "^ExpressionData.csv", recursive = TRUE, include.dirs = TRUE)
+
+
+impList <- lapply(expr_files, function(file) {
+  
+  dat <- read.csv(paste0(data_dir, file), row.names = 1)
+  dat_name <- dirname(file)
+  print(dat_name)
+  
+  
+  # remove erccs
+  ercc.idx <- grep("^ERCC-", rownames(dat))
+  if( length(ercc.idx) ) { dat <- dat[-ercc.idx,] }
+  
+  nGenes <- nrow(dat)
+  
+  # seurat object
+  so_dat <- CreateSeuratObject(counts = dat, min.cells = 1)
+  
+  # normalisation and scaling
+  so_dat <- NormalizeData(so_dat, assay = "RNA")
+  so_dat <- ScaleData(so_dat, assay = "RNA")
+  
+  # variance stabilisation
+  so_dat <- FindVariableFeatures(so_dat, selection.method = "vst")
+  
+  
+  mat <- as.matrix(so_dat@assays$RNA@data)
+  
+  # run SAVER on 20 cores
+  saver_res <- saver(mat, size.factor = 1, ncores = 20, estimates.only = F)
+  dat.saver <- as.data.frame(saver_res$estimate)
+  
+  
+  new_file <- gsub(".csv", "_saver_imputed.csv", file)
+  new_file <- paste0(data_dir, new_file)
+  write.csv(dat.saver, file = new_file, quote=F, row.names = T)
+  
+  return(new_file)
+  
+})
+
